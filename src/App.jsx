4a49fd964d2b8702ae52796f91b6353f42b11e30
avronilrajib <avronilrajib@gmail.com>
2024-08-13 03:03:42 +0600

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import Header from "./components/partials/Header/Header";
-import "remixicon/fonts/remixicon.css";
-import SideBar from "./components/Sidebar/SideBar";
+
+import { Route, Router, useLocation } from "react-router-dom";
+import Loader from "./common/Loader/Loader";
+import DefaultLayout from "./layout/DefaultLayout";
+import Dashboard from "./pages/Dashboard/Dashboard";
+
 function App() {
-  return (
-    <>
-      <Header />
-      <SideBar />
-    </>
+  const [loading, setLoading] = useState(true);
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <DefaultLayout>
+      {/* <Router>
+        <Route
+          path="/"
+          element={
+            <>
+              <Dashboard />
+            </>
+          }
+        />
+      </Router> */}
+    </DefaultLayout>
   );
 }
 
